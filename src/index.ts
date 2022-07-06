952ce26bc81ea68e12b5215aeb80c3d3ade57404
Markus Tammeoja, Anton Keks <markus@codeborne.com, anton@codeborne.com>
2022-07-06 13:59:01 +0300

    add .js extensions to imports in .ts files to produce valid native ES modules that can work in browser and node directly
    https://github.com/microsoft/TypeScript/issues/16577#issuecomment-754941937

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import type {Options} from './i18n'
-import {init as i18n, lang} from './i18n'
-import {init as formatters} from './formatters'
+import {init as i18n, lang} from './i18n.js'
+import {init as formatters} from './formatters.js'
 
-export * from './i18n'
-export * from './formatters'
+export * from './i18n.js'
+export * from './formatters.js'
 
 export async function init(i: Options) {
   await i18n(i)
